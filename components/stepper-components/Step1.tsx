2e6b174c397924d5a882d3f63f31ea77bfb70e85
elmars-hub <martinsifeanyi247@gmail.com>
2025-05-20 19:28:21 -0700

    feat: add Textarea component and update dependencies
    - Created a new Textarea component in `components/ui/textarea.tsx` with customizable styles.
    - Updated package dependencies in `package.json` and `package-lock.json`:
      - Added `@radix-ui/react-popover`, `@radix-ui/react-select`, `date-fns`, and `react-day-picker`.
      - Updated `@radix-ui/react-slot` to version `1.2.3`.

--- components/stepper-components/Step1.tsx
+++ components/stepper-components/Step1.tsx
@@ -8,7 +8,10 @@ const Step1 = ({ purpose, subtext }: Step1Props) => {
     <div className="mt-10">
       <h3 className="font-bold text-xl">{purpose}</h3>
       <p className="text-sm text-gray-500">{subtext}</p>
-      <div className="grid grid-cols-2 mt-5 gap-8"></div>
+      <div className="grid grid-cols-2 mt-5 gap-8">
+        <div className="mt-4"></div>
+        <div className="mt-4"></div>
+      </div>
     </div>
   );
 };
